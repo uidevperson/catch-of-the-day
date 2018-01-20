4bd742c28dc0ba083e3fed3647b3b1dde2cce88c
uidevperson <uidevthing@gmail.com>
2018-01-20 13:00:55 +0000

    added comment and building up form element

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,12 @@ import React from 'react';
 class StorePicker extends React.Component {
 	render(){
 		return (
-			<div>StorePicker Test</div>
+			<form className="store-selector">
+		{ /*	 comment FFS */ }
+				<h2>Please enter a store</h2>
+				<input type="text" required placeholder="Store name"/>
+				<button type="submit">Visit Store</button>
+			</form>
 		);
 	}
 }
