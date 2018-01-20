4a97f64ac826a1e5e123f157bd7ce976586624c8
uidevperson <uidevthing@gmail.com>
2018-01-20 12:39:58 +0000

    component test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+	render(){
+		return (
+			<div>StorePicker</div>
+		);
+	}
+}
+
+render(<StorePicker />, document.querySelector('#main'));
+// export default StorePicker;
