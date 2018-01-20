f5580a901df2e3f3908c3223fb40f6ffb731a949
uidevperson <uidevthing@gmail.com>
2018-01-20 18:28:10 +0000

    import css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 // class StorePicker extends React.Component {
 // 	render(){
