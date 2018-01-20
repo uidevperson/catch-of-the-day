ff6aa54a0edb8c0d5c7b1a4cba636047cb8fd581
uidevperson <uidevthing@gmail.com>
2018-01-20 12:43:45 +0000

    move test component to components folder and import into index file

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+// let's go!
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render(){
+		return (
+			<div>StorePicker Test</div>
+		);
+	}
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-class StorePicker extends React.Component {
-	render(){
-		return (
-			<div>StorePicker</div>
-		);
-	}
-}
+// class StorePicker extends React.Component {
+// 	render(){
+// 		return (
+// 			<div>StorePicker</div>
+// 		);
+// 	}
+// }
 
 render(<StorePicker />, document.querySelector('#main'));
 // export default StorePicker;
