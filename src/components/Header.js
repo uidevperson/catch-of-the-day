5a2c4b8f9e1f66156588380a5581700c86631c9f
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-02-02 16:28:13 +0000

    add props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 class Header extends React.Component {
 	render(){
+		console.log(this);
 		return (
 			<header className="top">
-				<h1>Catch of the Day:</h1>
-				<h3 className="tagline">Fill Me In</h3>
+				<h1>Catch 
+				<span className="ofThe">
+					<span className="of">of</span>
+					<span className="the">of</span>
+				</span>
+				Day
+				</h1>
+				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
 			</header>
 		)
 	}
