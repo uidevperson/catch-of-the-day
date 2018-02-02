5e76f1c7804bae0398c710a1f88c3cc786b30312
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-02-02 15:50:32 +0000

    update load component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-import App from  './components/App';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<StorePicker />, document.querySelector('#main'));
