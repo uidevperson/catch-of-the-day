d687d073fa09c291ef24b76fddced159f66330ec
uidevperson <uidevthing@gmail.com>
2018-02-05 20:52:29 +0000

    test react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import StorePicker from './components/StorePicker';
 import App from  './components/App';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match pattern="/store/:id store" component={App} />				
+			</div>
+		</BrowserRouter>
+	)
+	
+}
+
+render(<Root />, document.querySelector('#main'));
