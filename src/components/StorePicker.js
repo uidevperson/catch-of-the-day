c55e52fc4048ef2b0b00c5f6fb4aee341ad7ba72
uidevperson <uidevthing@gmail.com>
2018-02-07 21:21:25 +0000

    add constructor

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	constructor(){
+		super();
+		this.gotToStore = this.gotToStore.bind(this);
+	}
+
 	gotToStore(e) {
 		e.preventDefault()
-		console.log('test');
+		console.log(this.storeInput);
 		//first grab the text from the input
 		//second transition from / to /store/:storeId	
 	}
@@ -14,7 +19,11 @@ class StorePicker extends React.Component {
 			<form className="store-selector" onSubmit={this.gotToStore}>
 		{ /*	 comment FFS */ }
 				<h2>Please enter a store</h2>
-				<input type="text" required placeholder="Store name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store name" defaultValue={getFunName()} 
+					ref={(input) => {
+						this.storeInput = input;
+					}}
+				/>
 				<button type="submit">Visit Store</button>
 			</form>
 		);
