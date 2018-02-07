991151572fa63c7ca36636bb43c2df4f42aba929
uidevperson <uidevthing@gmail.com>
2018-02-07 21:00:25 +0000

    event

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	gotToStore() {
+		console.log('test');
+		//first grab the text from the input
+		//second transition from / to /store/:storeId	
+	}
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.gotToStore()}>
 		{ /*	 comment FFS */ }
 				<h2>Please enter a store</h2>
 				<input type="text" required placeholder="Store name" defaultValue={getFunName()}/>
