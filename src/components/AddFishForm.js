176ceb03676d73aba2631d74db03ca67f8fbddf8
uidevperson <uidevthing@gmail.com>
2018-02-08 19:54:47 +0000

    extract data from form using refs

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
-	render(){
+	createFish(e) {
+		e.preventDefault();
+		const fish = {
+			name: this.name.value,
+			price: this.price.value,
+			status: this.status.value,
+			desc: this.desc.value,
+			image: this.image.value,
+		}
+		console.log(fish);
+	}
+	render() {
 		return (
-			<form  className="fish-edit">
-				<input type="text" placeholder="Fish Name"/>
-				<input type="text" placeholder="Fish Price"/>
-				<select name="" id="">
+			<form  className="fish-edit" onSubmit={(e) => {this.createFish(e)}}>
+				<input ref={(input) => {this.name = input}} type="text" placeholder="Fish Name"/>
+				<input ref={(input) => {this.price = input}} type="text" placeholder="Fish Price"/>
+				<select ref={(input) => {this.status = input}} name="" id="">
 					<option value="available">Fresh!</option>
 					<option value="unavailable">Sold Out</option>
 				</select>
-				<textarea placeholder="Fish Desc"></textarea>
-				<input type="text" placeholder="Fish Image"/>
+				<textarea ref={(input) => {this.desc = input}} placeholder="Fish Desc"></textarea>
+				<input ref={(input) => {this.image = input}} type="text" placeholder="Fish Image"/>
 				<button type="submit">+ Add Item</button>
 			</form>
 		)
