d06cf05db4abee0940b436fbda8224cdd1f27bb9
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-02-09 16:05:50 +0000

    onclick event update app state

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
 	constructor() {
 		super();
 		//
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
 		// get initial state
 		this.state = {
 			fishes: {},
@@ -17,12 +19,18 @@ class App extends React.Component {
 
 	addFish(fish){
 		// update state
-		const fishes = { ...this.state.fishes };
+		const fishes = {...this.state.fishes};
 		// add new fish
 		const timeStamp = Date.now();
 		fishes[`fishes-${timeStamp}`] = fish;
 		// set state
 		this.setState({fishes});
+	}
+
+	loadSamples(){
+		this.setState({
+			fishes: sampleFishes
+		});
 	}
 
 	render(){
@@ -32,7 +40,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood market" />
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		);
 	}
