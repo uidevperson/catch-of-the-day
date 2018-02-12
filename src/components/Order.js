5dc93a3acbc3f16cffbbfcff3895edd02a767c7f
uidevperson <uidevthing@gmail.com>
2018-02-12 19:45:28 +0000

    calculate total

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 class Order extends React.Component {
 	render(){
-		const orderIds = 	Object.keys(this.props.order);	
+		const orderIds = 	Object.keys(this.props.order);
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const count = this.props.order[key];
+			const isAvailable = fish && fish.status === 'available';
+
+			if(isAvailable){
+				return prevTotal + (count * fish.price || 0);
+			} 
+
+			return prevTotal;
+		}, 0);	
+
 		return (
 			<div className="order-wrap">
 				<h2>Order</h2>
 				<p>{orderIds}</p>
+				{total}
 			</div>
 		)
 	}
