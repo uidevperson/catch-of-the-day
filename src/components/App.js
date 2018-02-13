80b3ebfa70053080045ed0e4b17d029b9a0e1b18
uidevperson <uidevthing@gmail.com>
2018-02-13 19:40:51 +0000

    connect with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,17 @@ class App extends React.Component {
 		}
 	}
 
+	componentWillMount(){
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+			,{
+				context: this,
+				state: 'fishes'
+			});
+	}
+	componentWillUnmount(){
+		base.removeBinding(this.ref);
+	}
+
 	addFish(fish){
 		// update state
 		const fishes = {...this.state.fishes};
