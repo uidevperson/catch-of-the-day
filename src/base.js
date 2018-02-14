41fb94b52c6d0155aca9c3f5827df617f3b7508d
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-02-14 21:18:25 +0000

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,5 +1,5 @@
 import Rebase from 're-base';
-
+//connect to firebase DB
 const base = Rebase.createClass({
 	apiKey: "AIzaSyCbW5PGS20QX8UeEd-cn4LOqFzLCt7JZcE",
 	authDomain: "catch-of-the-day-uidevthing.firebaseapp.com",
