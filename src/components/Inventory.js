6ae0474ec4bcb08a2ba86ea5d20605b0098f51b0
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-02-16 16:14:24 +0000

    Add files via upload

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,14 +2,32 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
-	// constructor(){
-	// 	super();
-	// 	this.renderInventory = this.renderInventory.bind(this);
-	// }
+
+	constructor(){
+		super();
+		this.renderInventory = this.renderInventory.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e, key){
+		const fish = this.props.fishes[key];
+		console.log(fish);
+	}
 
 	renderInventory(key){
+		const fish = this.props.fishes[key];
+
 		return (
-			 <p key={key}>{key}</p>
+			 <div className="fish-edit" key={key}>
+			 	<input name="name" value={fish.name} type="text" placeholder="Fish Name" onChange={(e) => this.handleChange(e, key)}/>
+			 	<input name="price" value={fish.price} type="text" placeholder="Fish Price"/>
+			 	<select name="status" value={fish.status} id="">
+			 		<option value="available">Fresh!</option>
+			 		<option value="unavailable">Sold Out</option>
+			 	</select>
+			 	<textarea name="desc" value={fish.desc} placeholder="Fish Desc"></textarea>
+			 	<input name="image" value={fish.image} type="text" placeholder="Fish Image"/>
+			 </div>
 		)
 	} 
 
