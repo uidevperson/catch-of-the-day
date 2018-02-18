e9c32ffbe286c7e49cf73e41253b60a6f686b54c
uidevperson <uidevthing@gmail.com>
2018-02-18 17:17:56 +0000

    css transition group test

--- src/components/Order.js
+++ src/components/Order.js
@@ -43,13 +43,20 @@ class Order extends React.Component {
 		return (
 			<div className="order-wrap">
 				<h2>Order</h2>
-				<ul className="order">
+
+				<CSSTransitionGroup 
+				className='order'
+				component='ul'
+				transitionName='order'
+				transitionEnterTimeout={500}
+				transitionLeaveTimeout={500}
+				>
 				{orderIds.map(this.renderOrder)}
 					<li className="total">
 						<strong>Total:</strong>
 						{formatPrice(total)}	
 					</li>
-				</ul>
+				</CSSTransitionGroup>
 			</div>
 		)
 	}
