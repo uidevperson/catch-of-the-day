794623b2a7f5c618733d66f8fa814b15f638c52d
uidevperson <uidevthing@gmail.com>
2018-02-18 17:59:09 +0000

    finished animations

--- src/components/Order.js
+++ src/components/Order.js
@@ -21,7 +21,17 @@ class Order extends React.Component {
 		return (
 			<li key={key}>
 				<span>
-					{count}lbs {fish.name}{removeButton}
+					<CSSTransitionGroup
+						component='span'
+						className='count'
+						transitionName='count'
+						transitionEnterTimeout={250}
+						transitionLeaveTimeout={250}
+					>
+						<span key={count}>{count}</span>
+					</CSSTransitionGroup>
+						lbs {fish.name}
+						{removeButton}
 				</span>
 				<span className="price">{formatPrice(count * fish.price)}</span>
 			</li>
